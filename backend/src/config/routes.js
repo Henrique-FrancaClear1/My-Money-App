594bece0ffdfd6f724b09135ab8270e0462055bf
Henrique França <henriqsf@gmail.com>
2022-12-06 11:57:52 -0300

    feat: Autenticação - Backend (terminado)

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -10,7 +10,7 @@ module.exports = function (server) {
     server.use('/api', protectedApi)
 
     protectedApi.use(auth)
-    
+
     const BillingCycle = require('../api/billingCycle/billingCycleService')
     BillingCycle.register(protectedApi, '/billingCycles')
     /*
@@ -19,7 +19,7 @@ module.exports = function (server) {
     const openApi = express.Router()
     server.use('/oapi', openApi)
 
-    const AuthService = require('../api/user/AuthService')
+    const AuthService = require('../api/user/authService')
     openApi.post('/login', AuthService.login)
     openApi.post('/signup', AuthService.signup)
     openApi.post('/validateToken', AuthService.validateToken)
