390cc5cdddd20597e2e61ae691fdab15409a0045
Henrique França <henriqsf@gmail.com>
2022-12-06 10:49:30 -0300

    feat: Autenticação - Backend (metodo login)

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -15,3 +15,34 @@ const sendErrorsFromDB = (res, dbErrors) => {
     _.forIn(dbErrors.errors, error => errors.push(error.message))
     return res.status(400).json({ errors })
 }
+
+//login do usuario
+const login = (req, res, next) => {
+    //Pegando a senha e o emai do usuario
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+
+    //BUca pelo usuario de acordo com o email (um usuario por email)
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            return sendErrorsFromDB(res, err)
+        } else if (user && bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign(user, env.authSecret, {
+                expiresIn: "1 day"
+            })
+            const { name, email } = user
+            res.json({ name, email, token })
+        } else {
+            return res.status(400).send({ errors: ['Usuário/Senha inválidos'] })
+        }
+    })
+}
+
+//metodo para validar o token
+const validateToken = (req, res, next) => {
+    const token = req.body.token || ''
+    
+    jwt.verify(token, env.authSecret, function (err, decoded) {
+        return res.status(200).send({ valid: !err })
+    })
+}
